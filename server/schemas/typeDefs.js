089cef75d2ef20f311d27cc016c492378eb83de7
mikedaleo <mikedaleo@outlook.com>
2024-08-06 21:43:47 -0400

    initial set up of typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const typeDefs = `
+input BookInput {
+    authors: [String!]
+    description: String!
+    bookId: String!
+    image: String!
+    link: String!
+    title: String!
+}
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String!
+    authors: [String!]
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+}
+
+type Auth {
+    token: String!
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: String!): User
+}`
+
+module.exports = typeDefs;
